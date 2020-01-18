1bcc0f6d03485041820ed5f6ef95959106b6ad1f
nothingholy <pershaevnikita@yandex.ru>
2020-01-18 22:40:51 +0300

    run fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,11 +2,5 @@
 
 set -ex
 
-pip3.7 install -r requirements.txt
-
-screen -XS alice-skill quit || true   # Stop current alice session
-screen -wipe || true                  # Remove dead screens
-
 cd code
-
-screen -dmS alice-skill python3.7 main.py
+python3.7 main.py
