c04633e2e6b4da2310e69b7e5b2a526fcbbd9ad8
nothingholy <pershaevnikita@yandex.ru>
2020-01-18 21:58:50 +0300

    run screen fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,9 +5,8 @@ set -ex
 pip3.7 install -r requirements.txt
 
 screen -XS alice-skill quit || true   # Stop current alice session
+screen -wipe
 
 cd code
-
-sleep 2
 
 screen -dmS alice-skill python3.7 main.py
